3e8e8dffaeecfbcc7804236211f57b8c577ae39e
davidclaveau <daveclaveau@gmail.com>
2021-07-14 13:47:57 -0700

    Add a Procfile and changes for heroku deployment

--- src/server/index.js
+++ src/server/index.js
@@ -432,6 +432,7 @@ app.get("/refresh_token", function (req, res) {
   });
 });
 
-const port = process.env.SERVER_PORT || 4000;
+const port = process.env.SERVER_PORT || 8000;
+
 app.listen(port);
 console.log(`Running a GraphQL API server at localhost:${port}/graphql`);
